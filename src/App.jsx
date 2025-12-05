a1e10348a2c67e8f1361e91fae5a3e5fe2433061
Deepak Kumar <deepak@example.com>
2025-12-06 00:10:40 +0530

    Update React data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import viteLogo from "./assets/react.svg"; // or remove if unused
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
