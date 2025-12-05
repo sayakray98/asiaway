660fedb5dde3a3b481c9af64447c91df82536686
Deepak Kumar <deepak@example.com>
2025-12-06 01:33:56 +0530

    Update React data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,30 +11,38 @@ export default function Header() {
         <nav className="navbar navbar-expand-lg p-0">
           <div className="container-fluid">
             <div className="row">
-              <div className="col-lg-4">
+              <div className="col-lg-4 col-md-6 col-8">
                 <Link
                   className="navbar-brand pl-5 ml-5"
                   to="/"
                   style={{ textAlign: "left" }}
                 >
-                  <img src={logo} alt="" style={{ width: "48%" }} />
+                  <img
+                    src={logo}
+                    alt=""
+                    style={{ width: "48%" }}
+                    className="logoimg"
+                  />
                 </Link>
-
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#navbarSupportedContent"
-                  aria-controls="navbarSupportedContent"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span className="navbar-toggler-icon"></span>
-                </button>
               </div>
 
-              <div className="col-lg-8 d-flex align-items-center justify-content-center">
+              <div className="col-lg-8 col-md-6 col-4 d-flex align-items-center justify-content-center">
                 <div className="navdetails">
+                  <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <div className="navbaricondiv">
+                      <span className="navbar-icons"></span>
+                      <span className="navbar-icons"></span>
+                      <span className="navbar-icons"></span>
+                    </div>
+                  </button>
                   <div
                     className="collapse navbar-collapse"
                     id="navbarSupportedContent"
@@ -99,7 +107,6 @@ export default function Header() {
                         </div>
                       </li>
 
-                     
                       <li className="nav-item">
                         <Link className="nav-link" to="/">
                           Our Fleets
